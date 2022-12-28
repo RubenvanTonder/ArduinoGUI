63eee2d42714bbd6ce40fea5faa409d805675bcd
RubenvanTonder <22569596@sun.ac.za>
2022-12-28 12:53:40 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace ArduinoGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.onButton = new System.Windows.Forms.Button();
+            this.offButton = new System.Windows.Forms.Button();
+            this.blinkButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM5";
+            // 
+            // onButton
+            // 
+            this.onButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onButton.Location = new System.Drawing.Point(206, 117);
+            this.onButton.Name = "onButton";
+            this.onButton.Size = new System.Drawing.Size(197, 108);
+            this.onButton.TabIndex = 0;
+            this.onButton.Text = "LED On";
+            this.onButton.UseVisualStyleBackColor = true;
+            this.onButton.Click += new System.EventHandler(this.onButton_Click);
+            // 
+            // offButton
+            // 
+            this.offButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.offButton.Location = new System.Drawing.Point(427, 117);
+            this.offButton.Name = "offButton";
+            this.offButton.Size = new System.Drawing.Size(197, 108);
+            this.offButton.TabIndex = 1;
+            this.offButton.Text = "LED Off";
+            this.offButton.UseVisualStyleBackColor = true;
+            this.offButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // blinkButton
+            // 
+            this.blinkButton.Location = new System.Drawing.Point(206, 263);
+            this.blinkButton.Name = "blinkButton";
+            this.blinkButton.Size = new System.Drawing.Size(197, 117);
+            this.blinkButton.TabIndex = 2;
+            this.blinkButton.Text = "blinkLED";
+            this.blinkButton.UseVisualStyleBackColor = true;
+            this.blinkButton.Click += new System.EventHandler(this.blinkButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(206, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.blinkButton);
+            this.Controls.Add(this.offButton);
+            this.Controls.Add(this.onButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button onButton;
+        private System.Windows.Forms.Button offButton;
+        private System.Windows.Forms.Button blinkButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArduinoGUI
+{
+    public partial class Form1 : Form
+    {
+        char input;
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Send Command to the arduino to turn off LED
+            serialPort1.Write("a");
+        }
+
+        private void onButton_Click(object sender, EventArgs e)
+        {
+            // Send Command to the arduino to turn on LED
+            serialPort1.Write("A");
+            Console.WriteLine("Hello World");
+        }
+
+        private void blinkButton_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("b");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("t");
+            Read();
+        }
+        public void Read()
+        {
+            input = (char)serialPort1.ReadChar();
+            if ( input == 'H') {
+                Console.WriteLine(input);
+                textBox1.Text = input.ToString();
+                textBox1.BackColor = Color.Black;
+            }
+            
+        }
+    }
+}
