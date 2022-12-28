34c8d971d6d57dee87e78e1ba3d7695e7e0e1975
RubenvanTonder <22569596@sun.ac.za>
2022-12-28 18:22:03 +0200

    Added left,right,up and down buttons to move ball around in LED display

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,8 +32,15 @@
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
             this.onButton = new System.Windows.Forms.Button();
             this.offButton = new System.Windows.Forms.Button();
-            this.blinkButton = new System.Windows.Forms.Button();
+            this.moveRightButton = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.moveLeftButton = new System.Windows.Forms.Button();
+            this.moveUpButton = new System.Windows.Forms.Button();
+            this.moveDownButton = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // serialPort1
@@ -63,37 +70,127 @@
             this.offButton.UseVisualStyleBackColor = true;
             this.offButton.Click += new System.EventHandler(this.button2_Click);
             // 
-            // blinkButton
+            // moveRightButton
             // 
-            this.blinkButton.Location = new System.Drawing.Point(418, 12);
-            this.blinkButton.Name = "blinkButton";
-            this.blinkButton.Size = new System.Drawing.Size(189, 108);
-            this.blinkButton.TabIndex = 2;
-            this.blinkButton.Text = "blinkLED";
-            this.blinkButton.UseVisualStyleBackColor = true;
-            this.blinkButton.Click += new System.EventHandler(this.blinkButton_Click);
+            this.moveRightButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moveRightButton.Location = new System.Drawing.Point(815, 259);
+            this.moveRightButton.Name = "moveRightButton";
+            this.moveRightButton.Size = new System.Drawing.Size(189, 108);
+            this.moveRightButton.TabIndex = 2;
+            this.moveRightButton.Text = "Move Right";
+            this.moveRightButton.UseVisualStyleBackColor = true;
+            this.moveRightButton.Click += new System.EventHandler(this.blinkButton_Click);
             // 
             // panel1
             // 
             this.panel1.Location = new System.Drawing.Point(12, 126);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(934, 456);
+            this.panel1.Size = new System.Drawing.Size(595, 456);
             this.panel1.TabIndex = 3;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // moveLeftButton
+            // 
+            this.moveLeftButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moveLeftButton.Location = new System.Drawing.Point(613, 259);
+            this.moveLeftButton.Name = "moveLeftButton";
+            this.moveLeftButton.Size = new System.Drawing.Size(189, 108);
+            this.moveLeftButton.TabIndex = 4;
+            this.moveLeftButton.Text = "Move Left";
+            this.moveLeftButton.UseVisualStyleBackColor = true;
+            this.moveLeftButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // moveUpButton
+            // 
+            this.moveUpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moveUpButton.Location = new System.Drawing.Point(712, 136);
+            this.moveUpButton.Name = "moveUpButton";
+            this.moveUpButton.Size = new System.Drawing.Size(189, 108);
+            this.moveUpButton.TabIndex = 5;
+            this.moveUpButton.Text = "Move Up";
+            this.moveUpButton.UseVisualStyleBackColor = true;
+            this.moveUpButton.Click += new System.EventHandler(this.moveUpButton_Click);
+            // 
+            // moveDownButton
+            // 
+            this.moveDownButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moveDownButton.Location = new System.Drawing.Point(712, 387);
+            this.moveDownButton.Name = "moveDownButton";
+            this.moveDownButton.Size = new System.Drawing.Size(189, 108);
+            this.moveDownButton.TabIndex = 6;
+            this.moveDownButton.Text = "Move Down";
+            this.moveDownButton.UseVisualStyleBackColor = true;
+            this.moveDownButton.Click += new System.EventHandler(this.moveDownButton_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(665, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(107, 29);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Connect";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.ForeColor = System.Drawing.Color.Red;
+            this.statusLabel.Location = new System.Drawing.Point(724, 57);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(109, 25);
+            this.statusLabel.TabIndex = 8;
+            this.statusLabel.Text = "Disconnect";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(778, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(130, 29);
+            this.radioButton2.TabIndex = 9;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Disconnect";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(644, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 25);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Status:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(958, 594);
+            this.ClientSize = new System.Drawing.Size(1006, 594);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.moveDownButton);
+            this.Controls.Add(this.moveUpButton);
+            this.Controls.Add(this.moveLeftButton);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.blinkButton);
+            this.Controls.Add(this.moveRightButton);
             this.Controls.Add(this.offButton);
             this.Controls.Add(this.onButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -102,8 +199,15 @@
         private System.IO.Ports.SerialPort serialPort1;
         private System.Windows.Forms.Button onButton;
         private System.Windows.Forms.Button offButton;
-        private System.Windows.Forms.Button blinkButton;
+        private System.Windows.Forms.Button moveRightButton;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button moveLeftButton;
+        private System.Windows.Forms.Button moveUpButton;
+        private System.Windows.Forms.Button moveDownButton;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -19,33 +19,37 @@ namespace ArduinoGUI
         int spaceBetweenPixels = 20;
         char input;
         string ledMovement = "0000";
-        int ballStartPosX = 0;
-        int ballStartPosY = 0;
+        int ballPosX = 0;
+        int ballPosY = 0;
+        int ballPrevPosX;
+        int ballPrevPosY;
         int one = 1;
         private Timer timer1;
+        bool serialOpen;
 
         public Form1()
         {
             InitializeComponent();
-            serialPort1.Open();
             InitTimer();
- 
+
         }
         
         public void InitTimer()
         {
             timer1 = new Timer();
             timer1.Tick += new EventHandler(timer1_Tick);
-            timer1.Interval = 1000; // in miliseconds
+            timer1.Interval = 10; // in miliseconds
             timer1.Start();
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
             Graphics g_0 = this.panel1.CreateGraphics();
-            SolidBrush b = new SolidBrush(Color.Green);
+            SolidBrush b = new SolidBrush(Color.Black);
+            g_0.FillEllipse(b, startX + ballPrevPosX * spaceBetweenPixels, startY + ballPrevPosY * spaceBetweenPixels, 10, 10);
 
-            g_0.FillEllipse(b, startX + ballStartPosX * spaceBetweenPixels, startY + ballStartPosY * spaceBetweenPixels, 10, 10);
+            b.Color = Color.Green;
+            g_0.FillEllipse(b, startX + ballPosX * spaceBetweenPixels, startY + ballPosY * spaceBetweenPixels, 10, 10);
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -58,37 +62,48 @@ namespace ArduinoGUI
         {
             // Send Command to the arduino to turn on LED
             serialPort1.Write("A");
-            ballStartPosX++;
-
-
         }
 
-        private void blinkButton_Click(object sender, EventArgs e)
-        {
-            serialPort1.Write("b");
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        public void MoveRight()
         {
             
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            serialPort1.Write("t");
-            Read();
-        }
-        public void Read()
-        {
-            input = (char)serialPort1.ReadChar();
-            if ( input == 'H') {
-                Console.WriteLine(input);
-
+            if (ballPosX < 9)
+            {
+                ballPrevPosX = ballPosX;
+                ballPrevPosY = ballPosY;
+                ballPosX++;
             }
-            
         }
-
+        public void MoveLeft()
+        {
+            
+            if (ballPosX > 0)
+            {
+                ballPrevPosX = ballPosX;
+                ballPrevPosY = ballPosY;
+                ballPosX--;
+            }
+        }
+        public void MoveUp()
+        {
+            
+            if (ballPosY > 0)
+            {
+                ballPrevPosY = ballPosY;
+                ballPrevPosX = ballPosX;
+                ballPosY--;
+            }
+        }
+        public void MoveDown()
+        {
+            
+            if (ballPosY < 9)
+            {
+                ballPrevPosY = ballPosY;
+                ballPrevPosX = ballPosX;
+                ballPosY++;
+            }
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
@@ -107,6 +122,46 @@ namespace ArduinoGUI
                 
             }
             
+        }
+        private void blinkButton_Click(object sender, EventArgs e)
+        {
+            MoveRight();
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            MoveLeft();
+        }
+
+        private void moveUpButton_Click(object sender, EventArgs e)
+        {
+            MoveUp();
+        }
+
+        private void moveDownButton_Click(object sender, EventArgs e)
+        {
+            MoveDown();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (serialOpen == false)
+            {
+                serialPort1.Open();
+                statusLabel.Text = "Connect";
+                statusLabel.ForeColor = Color.Green;
+                serialOpen = true;
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (serialOpen == true)
+            {
+                serialPort1.Close();
+                statusLabel.Text = "Disconnect";
+                statusLabel.ForeColor = Color.Red;
+                serialOpen = false;
+            }
         }
     }
 }
